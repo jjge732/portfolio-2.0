8c5f41b1f448db322caaee1c13200576bc4c2250
John Eastwood <jjge732@gmail.com>
2020-05-30 12:32:04 -0500

    Added appspec.yml for use by CodeDeploy

---
+++ scripts/build.sh
@@ -0,0 +1,8 @@
+sudo yum update
+curl --silent --location https://rpm.nodesource.com/setup_10.x | sudo bash
+sudo yum install nodejs
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+sudo rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
+sudo yum install yarn
+yarn install
+yarn build
