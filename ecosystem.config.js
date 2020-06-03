3ab8f439fd0a4767c7604925e322244423ce637a
John Eastwood <jjge732@gmail.com>
2020-06-03 07:45:19 -0500

    Started using pm2 for the process management of the app

---
+++ ecosystem.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  apps : [{
+    script: 'scripts/start_server.sh',
+    watch: '.'
+  }],
+};
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router'
 
 import Container from '../components/container'
 
+
 export default function Home() {
   const router = useRouter();
   return <Container router={router}/>
